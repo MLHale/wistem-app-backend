158d6c373239212690f8933c070eb054d37e889e
nwood <nwood01@unomaha.edu>
2017-12-05 20:18:01 -0600

    stem field list and detail tested.

--- api/controllers.py
+++ api/controllers.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.renderers import JSONRenderer
 # from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django_filters.rest_framework import DjangoFilterBackend
@@ -271,7 +272,7 @@ class StemFieldDetail(APIView):
             return Response({'success': False, 'error': e}, status=status.HTTP_400_BAD_REQUEST)
 
         stemfield.save()
-        print('Stem Field updated: ' + stemfield.title)
+        print('Stem Field updated: ' + stemfield.field)
         return Response({'success': True}, status=status.HTTP_200_OK)
 
     def delete(self, request, id=None):
