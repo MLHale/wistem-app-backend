c0f6113cd9f1a9c7fc04fee15d4fa31d182d30ab
Matt Hale <mlhale@unomaha.edu>
2017-12-08 13:09:16 -0600

    updated serializer to show example of retrieving a single model instance

--- api/serializers.py
+++ api/serializers.py
@@ -76,57 +76,61 @@ class AwardSerializerAdmin(serializers.Serializer):
     created_by = ProfileSerializer()
     created_on = serializers.DateTimeField()
 
-class AwardSerializer(serializers.Serializer):
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        depth = 1
+        fields = "__all__"
     # Applicant Type Choices
-    STAFF = 'staff'
-    PROF_FAC = 'faculty_professor'
-    TENURED_FAC = 'tenured_faculty'
-    NON_TENURED_FAC = 'non_tenured_faculty'
-    PHD = 'phd'
-    GRAD_FAC = 'graduate_faculty'
-    GRAD_STUD = 'graduate_student'
-    UGRAD_STUD = 'undergraduate_student'
-    TYPE_OTHER = 'other'
-    APPLICANT_TYPE_CHOICES = (
-        (STAFF, 'Staff'),
-        (PROF_FAC, 'Faculty - Professor'),
-        (TENURED_FAC, 'Faculty - Tenured'),
-        (NON_TENURED_FAC, 'Faculty - Non-Tenured'),
-        (GRAD_FAC, 'Faculty - Graduate'),
-        (PHD, 'PhD Required'),
-        (GRAD_STUD, 'Student - Graduate'),
-        (UGRAD_STUD, 'Student - Undergraduate'),
-        (TYPE_OTHER, 'Other')
-    )
-
-    # Award Source choices
-    FED = 'federal'
-    LOCAL = 'local'
-    STATE = 'state'
-    PRIV = 'private_industry'
-    INT = 'internal'
-    SOURCE_OTHER = 'other'
-    AWARD_SOURCE_CHOICES = (
-        (FED, 'Federal Government'),
-        (STATE,'State Government'),
-        (LOCAL, 'Local Government'),
-        (INT, 'Internal'),
-        (PRIV, 'Private Industry'),
-        (SOURCE_OTHER, 'Other')
-    )
-
-    title = serializers.CharField(max_length=1000, allow_blank=False)
-    description = serializers.CharField(allow_blank=True)
-    award_link = serializers.URLField(max_length=1000, allow_blank=False)
-    sponsor_org = serializers.CharField(max_length=1000, allow_blank=False)
-    stem_field = StemFieldSerializer(many=True)
-    recurring = serializers.BooleanField()
-    nom_req = serializers.BooleanField()
-    recur_interval = serializers.CharField(max_length=1000, allow_blank=False)
-    open_date = serializers.DateTimeField()
-    nom_deadline = serializers.DateTimeField()
-    subm_deadline = serializers.DateTimeField()
-    applicant_type = serializers.ChoiceField(choices=APPLICANT_TYPE_CHOICES)
-    award_purpose = AwardPurposeSerializer(many=True)
-    additional_info = serializers.CharField(allow_blank=True)
-    source = serializers.ChoiceField(choices=AWARD_SOURCE_CHOICES)
+    # STAFF = 'staff'
+    # PROF_FAC = 'faculty_professor'
+    # TENURED_FAC = 'tenured_faculty'
+    # NON_TENURED_FAC = 'non_tenured_faculty'
+    # PHD = 'phd'
+    # GRAD_FAC = 'graduate_faculty'
+    # GRAD_STUD = 'graduate_student'
+    # UGRAD_STUD = 'undergraduate_student'
+    # TYPE_OTHER = 'other'
+    # APPLICANT_TYPE_CHOICES = (
+    #     (STAFF, 'Staff'),
+    #     (PROF_FAC, 'Faculty - Professor'),
+    #     (TENURED_FAC, 'Faculty - Tenured'),
+    #     (NON_TENURED_FAC, 'Faculty - Non-Tenured'),
+    #     (GRAD_FAC, 'Faculty - Graduate'),
+    #     (PHD, 'PhD Required'),
+    #     (GRAD_STUD, 'Student - Graduate'),
+    #     (UGRAD_STUD, 'Student - Undergraduate'),
+    #     (TYPE_OTHER, 'Other')
+    # )
+    #
+    # # Award Source choices
+    # FED = 'federal'
+    # LOCAL = 'local'
+    # STATE = 'state'
+    # PRIV = 'private_industry'
+    # INT = 'internal'
+    # SOURCE_OTHER = 'other'
+    # AWARD_SOURCE_CHOICES = (
+    #     (FED, 'Federal Government'),
+    #     (STATE,'State Government'),
+    #     (LOCAL, 'Local Government'),
+    #     (INT, 'Internal'),
+    #     (PRIV, 'Private Industry'),
+    #     (SOURCE_OTHER, 'Other')
+    # )
+    #
+    # title = serializers.CharField(max_length=1000, allow_blank=False)
+    # description = serializers.CharField(allow_blank=True)
+    # award_link = serializers.URLField(max_length=1000, allow_blank=False)
+    # sponsor_org = serializers.CharField(max_length=1000, allow_blank=False)
+    # stem_field = StemFieldSerializer(many=True)
+    # recurring = serializers.BooleanField()
+    # nom_req = serializers.BooleanField()
+    # recur_interval = serializers.CharField(max_length=1000, allow_blank=False)
+    # open_date = serializers.DateTimeField()
+    # nom_deadline = serializers.DateTimeField()
+    # subm_deadline = serializers.DateTimeField()
+    # applicant_type = serializers.ChoiceField(choices=APPLICANT_TYPE_CHOICES)
+    # award_purpose = AwardPurposeSerializer(many=True)
+    # additional_info = serializers.CharField(allow_blank=True)
+    # source = serializers.ChoiceField(choices=AWARD_SOURCE_CHOICES)
